f8597374e5c4e3a53a04f086d8db2912cfa156fa
remy <remy.lille@laposte.net>
2025-05-19 15:10:28 +0200

    projet todo

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,14 @@
 package org.example;
 
+
+import org.example.ihm.Ihm;
+import org.example.service.TodoService;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        TodoService service = null; // remplacer null par une instanciation d'une classe respectant l'interface TodoService
+        Ihm ihm = new Ihm(service);
+        ihm.start();
     }
 }
